fd4f94e101ab029dfdb3ff04d85785e1c193bcaa
Aron <blazte.aster@gmail.com>
2023-02-03 09:09:30 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export default function App() {
+  return (
+    <FrequentlyAskedQuestion
+      question="What does “CSS” stand for?"
+      answer="Cool Styling Strategy"
+    />
+  );
+}
+
+const Details = styled.details`
+  font-size: 1rem;
+  color: hsl(0deg 0% 25%);
+  margin: 32px;
+  border: 2px solid;
+  border-radius: 4px;
+`;
+
+const Summary = styled.summary`
+  cursor: pointer;
+  padding: 12px 16px;
+  outline-offset: 5px;
+`;
+
+const Answer = styled.div`
+  background: hsl(0deg 0% 92.5%);
+  border-radius: 0 0 4px 4px;
+  padding: 12px 17px;
+  font-style: italic;
+`;
+
+function FrequentlyAskedQuestion({ question, answer }) {
+  return (
+    <Details className="faq">
+      <Summary>{question}</Summary>
+      <Answer className="answer">{answer}</Answer>
+    </Details>
+  );
+}
